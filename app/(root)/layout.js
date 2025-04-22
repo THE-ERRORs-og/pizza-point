1abb3f791fc479689fc63dbe098555c0cb544867
Govind Ji <jigovind556@gmail.com>
2025-04-23 00:19:48 +0530

    initialised project and created folder structure

---
+++ app/(root)/layout.js
@@ -0,0 +1,15 @@
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
+import React from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="">
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
